3a0502bf94dec67683d6d93987013a0c9ba24ef1
jessicalam <j.lam@mail.com>
2017-02-07 16:17:01 -0500

    Yay completed project maybe

---
+++ dating_profile.js
@@ -0,0 +1,32 @@
+var firstnameInput = document.getElementById('firstname')
+var lastnameInput = document.getElementById('lastname')
+var descriptionInput = document.getElementById('description')
+var emailInput = document.getElementById('email')
+var phoneInput = document.getElementById('phone')
+var paragraphInput = document.getElementById('generated-paragraph')
+var codedparagraphInput = document.getElementById('htmled-paragraph')
+
+var updateParagraphInput = function () {
+  var firstname = firstnameInput.value
+  var lastname = lastnameInput.value
+  var description = descriptionInput.value
+  var email = emailInput.value
+  var phone = phoneInput.value
+
+  paragraphInput.innerHTML =
+    '<h1>Hi, my name is ' + firstname + ' ' + lastname + '!</h1>' +
+    '\n' + description + '\n' +
+    'If you\'re interested in a date, you can email me at ' + '<a href="mailto:"' + email + ' target=_blank">' + email + '</a>' +
+    ' or give me a call at ' + '<a href="tel:"' + phone + ' target="_blank">' + phone + '</a>'
+
+  codedparagraphInput.textContent =
+    '<h1>Hi, my name is ' + firstname + ' ' + lastname + '!</h1>' +
+    '\n' + description + '\n' +
+    'If you\'re interested in a date, you can email me at ' + '<a href="mailto:"' + email + ' target=_blank">' + email + '</a>' +
+    ' or give me a call at ' + '<a href="tel:"' + phone + ' target="_blank">' + phone + '</a>'
+}
+firstnameInput.addEventListener('input', updateParagraphInput)
+lastnameInput.addEventListener('input', updateParagraphInput)
+descriptionInput.addEventListener('input', updateParagraphInput)
+emailInput.addEventListener('input', updateParagraphInput)
+phoneInput.addEventListener('input', updateParagraphInput)
